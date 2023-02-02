417af872bbb695b0ce6f225274ca9b8a6ccc1ae4
Javier Nicolás González <89177014+nicolasgonzalez78837@users.noreply.github.com>
2023-02-01 23:16:07 -0300

    Update main.c

--- Lecture 1/main.c
+++ Lecture 1/main.c
@@ -6,14 +6,3 @@ int main(void)
   string answer = get_string("What´s your name: ");
   printf("Hello, %s\n", answer);
 }
-
-
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-  string first = get_string("What´s your first name: ");
-  string last = get_string("What´s your last name: ");
-  printf("Hello, %s %s\n", first, last);
-}
